9453de67b54e2fb10456f6cb5ea3a0ba768afe3f
mapAWro <konradkluwak@gmail.com>
2020-08-12 23:21:13 +0200

    Add bacic function to crop file

---
+++ RDCLabs/dataprocesing.py
@@ -0,0 +1,24 @@
+import btk
+
+
+def cropp_c3dfile(eventsFrame, filename):
+    reader = btk.btkAcquisitionFileReader()
+    reader.SetFilename(filename)
+    reader.Update()
+    acq = reader.GetOutput()
+    
+    writer = btk.btkAcquisitionFileWriter()
+    
+    for i in range(0, len(eventsFrame)):
+        clone = acq.Clone();
+        clone.ResizeFrameNumberFromEnd(acq.GetLastFrame() - eventsFrame[i][0] + 1)
+        clone.ResizeFrameNumber(eventsFrame[i][1] - eventsFrame[i][0] + 1)
+        clone.SetFirstFrame(eventsFrame[i][0])
+            
+        clone.ClearEvents()
+        for e in btk.Iterate(acq.GetEvents()):
+            if ((e.GetFrame() > clone.GetFirstFrame()) and (e.GetFrame() < clone.GetLastFrame())):
+                  clone.AppendEvent(e)
+        writer.SetInput(clone)
+        writer.SetFilename(filename.split('.')[0] + '-K' + str(i+1) + '.c3d')
+        writer.Update()
